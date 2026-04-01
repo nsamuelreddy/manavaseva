a519243f6714ff88adbc04ddadb57ad00887d0f2
nsamuelreddy <nareshsamuelreddy@gmail.com>
2026-04-02 02:40:55 +0530

    fix: use HashRouter to prevent Vercel deep-link 404

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import { AuthProvider } from './context/AuthContext'
@@ -8,12 +8,12 @@ import { DataProvider } from './context/DataContext'
 
 createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <AuthProvider>
                 <DataProvider>
                     <App />
                 </DataProvider>
             </AuthProvider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 )
